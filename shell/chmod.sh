fb981acefff1c45bc9f74ead431086389400a791
PhoenixWhitefire <phoenixwhitefire2000@gmail.com>
2026-06-22 19:40:29 +0530

    Make `shell/chmod.sh` itself executable

--- shell/chmod.sh
+++ shell/chmod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-chmod +x shell/dependencies.sh shell/configure.sh shell/build.sh shell/docs.sh shell/tests.sh shell/buildprofiler.sh shell/update.sh
+chmod +x shell/dependencies.sh shell/configure.sh shell/build.sh shell/docs.sh shell/tests.sh shell/buildprofiler.sh shell/update.sh shell/chmod.sh
