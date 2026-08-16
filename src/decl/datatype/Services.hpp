876075542f4d8696bbc4af6d006fbbee41c19606
PhoenixWhitefire <phoenixwhitefire2000@gmail.com>
2026-08-16 12:40:32 +0530

    add `Services.hpp` to tracking

---
+++ src/decl/datatype/Services.hpp
@@ -0,0 +1,21 @@
+// Services.hpp, 16/08/2026
+#pragma once
+
+#include <string_view>
+
+const std::string_view s_DataModelServices[] = {
+    "AssetManager",
+    "Collections",
+    "Engine",
+    "Environment",
+    "History",
+    "Interface",
+    "Logging",
+    "Network",
+    "Physics",
+    "PlayerInput",
+    "Renderer",
+    "DeveloperTools",
+    "ScriptEngine",
+    "Workspace",
+};
